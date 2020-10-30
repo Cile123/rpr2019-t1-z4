45d3bdd9d6ab362507f93e75441ec97a07635c91
Eldar Civgin <ecivgin1@etf.unsa.ba>
2020-10-30 17:20:10 +0100

    Uradjena klasa Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -4,19 +4,29 @@ public class Supermarket {
     private Artikl[] Artikli = new Artikl[1000];
     private static int brojArtikala = 0;
 
-    public void dodajArtikl(Artikl temp) {
+    public boolean dodajArtikl(Artikl temp) {
         if (brojArtikala > 999) {
-            System.out.println("Dostignut maximalan broj artikala");
-            return;
+            return false;
         }
         Artikli[brojArtikala] = temp;
         brojArtikala = brojArtikala + 1;
+        return true;
     }
-
     public Artikl[] getArtikli() {
         return Artikli;
     }
-
     public Artikl izbaciArtiklSaKodom(String kod) {
+        if (Artikli == null)
+            return null;
+
+        for (int i = 0; i < brojArtikala; i++) {
+            if (Artikli[i].getKod().equals(kod)) {
+                for (int j = i; j < brojArtikala-1; j++)
+                    Artikli[j] = Artikli[j+1];
+                brojArtikala = brojArtikala - 1;
+                return Artikli[brojArtikala-1];
+            }
+        }
+        return null;
     }
 }
